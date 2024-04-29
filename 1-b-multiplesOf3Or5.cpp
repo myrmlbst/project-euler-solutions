2e225906c97a6a54430c43e0650cbb48825aaefd
Myriam <160158729+myrmlbst@users.noreply.github.com>
2024-04-30 00:31:22 +0300

    Update 1-b-multiplesOf3Or5.cpp

--- 1-b-multiplesOf3Or5.cpp
+++ 1-b-multiplesOf3Or5.cpp
@@ -2,7 +2,17 @@
 
 // second method: seperate the %
 
-int multiple() {
+/* 
+LINE OF THINKING:
+In the previous method, we iterate through all numbers from 1 to 1000. That approach isn't necessary, 
+as we know that we only need to go over the numbers that are divisible by 3 and 5. So here, we only 
+iterate through the numbers which we know satisfy our requirements and skip out on the rest.
+
+In the second for-loop of our solution function, we skip out the numbers that are divisible by both 5
+and 3 in order to avoid duplicates in the two loops.
+*/
+
+int solution() {
     int count = 0;
     
     for (int i=3; i<1000; i+=3) {
@@ -21,6 +31,6 @@ int multiple() {
 
 
 int main() {
-    std::cout<< multiple() << std::endl;
+    std::cout<< solution() << std::endl;
     return 0;
 }

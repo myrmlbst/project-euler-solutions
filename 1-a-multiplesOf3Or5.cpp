1a6ea62ae51e3a9888761a270ae7678950fea932
Myriam <160158729+myrmlbst@users.noreply.github.com>
2024-05-10 17:49:07 +0300

    modified comments to adhere to project euler's code sharing rules

--- 1-a-multiplesOf3Or5.cpp
+++ 1-a-multiplesOf3Or5.cpp
@@ -1,10 +1,14 @@
-exercise 1 approach 1
-
+// exercise 1 approach 1
 // first method: brute-force approach
 
 int multiple() {
     int count = 0;
-    
+
+    /* 
+    we iterate through every element from 0 to 1000 to check
+    whether or not they are divisible by 3 & 5. If they are, 
+    we increment the counter by 1.
+    */
     for (int i=0; i<1000; i++) {
         if (i%3==0 || i%5==0) {
             count += i;
